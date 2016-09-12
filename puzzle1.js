6aa4f169249f0831b55cb6dd821d5c725a05bee6
Dalezio <daliliu1996@gmail.com>
2016-09-12 00:31:07 -0700

    Create puzzle1.js

---
+++ puzzle1.js
@@ -0,0 +1,21 @@
+function moveUnits(totalStep, direction){
+  for(var stepCount = 0; stepCount < totalStep; stepCount = stepCount + 1){
+    if (direction == "down"){
+      down();
+    }
+    if (direction == "up"){
+      up();
+    }
+    if (direction == "left"){
+      left();
+    }
+    if (direction == "right"){
+      right();
+  	}
+  }
+}
+
+moveUnits(7, "down");
+moveUnits(5, "right");
+moveUnits(2, "up");
+moveUnits(3, "right");
